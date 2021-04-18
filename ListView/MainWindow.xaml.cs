88bf4698df40dec93272b5c4df5939a4441381fd
Petrouy <petros3014@gmail.com>
2021-04-18 22:43:52 +0200

    Přidejte soubory projektu.

---
+++ ListView/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Osoba.InitOsoby();
+            LVOsoby.DataContext = Osoba.Osoby;
+        }
+
+        private void LVOsoby_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Osoba o = ((KeyValuePair<string, Osoba>)(sender as System.Windows.Controls.ListView).SelectedItem).Value;
+            DataContext = o;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LVOsoby.SelectedIndex = 0;
+        }
+    }
+}
---
+++ ListView/Osoba.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView
+{
+    class Osoba : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static Dictionary<string, Osoba> Osoby = new Dictionary<string, Osoba>();
+
+
+
+        private string jmeno;
+        public string Jmeno
+        {
+            get => jmeno;
+            set { jmeno = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Jmeno")); }
+        }
+
+
+        private string prijmeni;
+        public string Prijmeni
+        {
+            get => prijmeni;
+            set { prijmeni = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Prijmeni")); }
+        }
+
+
+
+        private int datumNarozeni;
+        public int DatumNarozeni
+        {
+            get => datumNarozeni;
+            set { datumNarozeni = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DatumNarozeni")); }
+        }
+
+
+
+        private string rodneCislo;
+        public string RodneCislo
+        {
+            get => rodneCislo;
+            set { rodneCislo = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RodneCislo")); }
+        }
+        
+
+
+
+
+
+
+        public static void InitOsoby()
+        {
+            Osoby["prvni"] = new Osoba
+            {
+                Jmeno = "Petr",
+                Prijmeni = "Novák",
+                DatumNarozeni = 05122000,
+                RodneCislo = "001205/0235"               
+            };
+
+
+            Osoby["druhy"] = new Osoba
+            {
+                Jmeno = "Ivan",
+                Prijmeni = "Skála",
+                DatumNarozeni = 21061995,
+                RodneCislo = "950621/8924"
+            };
+
+
+            Osoby["treti"] = new Osoba
+            {
+                Jmeno = "Jan",
+                Prijmeni = "Ořech",
+                DatumNarozeni = 30021986,
+                RodneCislo = "860230/5723"
+            };
+        }
+
+
+
+
+    }
+}
